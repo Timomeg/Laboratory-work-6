eb0d33fe8ed3027527b2182a049cfa9134c120e0
Timofey Rotin <162833896+Timomeg@users.noreply.github.com>
2024-11-19 13:22:48 +0300

    sort update

--- Sort.cpp
+++ Sort.cpp
@@ -2,4 +2,17 @@
 
 void Sort(std::list<int>& nums) {
 
+    auto size_vect = nums.begin();
+    while (size_vect != nums.end()) {
+        auto max_vect = size_vect;
+        for(auto iter {size_vect}; iter != nums.end(); ++iter) {
+            if (*iter > *max_vect) {
+                max_vect = iter;
+                *max_vect = *iter;
+                //std::cout << *max_vect << std::endl;
+            }
+        }
+        std::swap(*max_vect, *size_vect);
+        ++size_vect;
+    }
 }
